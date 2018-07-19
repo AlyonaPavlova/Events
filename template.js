f6d84994368dd236e0c2ecd386e5a0a2933d1175
Unknown <alyonabill3@mail.ru>
2018-07-19 10:28:35 +0300

    myPromise was added

--- template.js
+++ template.js
@@ -3,23 +3,34 @@
 const fs = require("fs");
 const mustache = require("mustache");
 
-function promiseAdd () {
-  return new Promise((resolve, reject) => {
-    fs.readFile("./data.json", (err, text) => {
-      if (err) throw err;
-      let parse = JSON.parse(text);
-      return parse;
-    })
-      .then(parse => {
-        let result = mustache.render(string, parse);
-        return result;
-      })
-      .then(result => {
-        fs.writeFile(("build2.html", result, (err) => {
-          if (err) throw err;
-        })
-      })
+const myPromise = new Promise((resolve, reject) => {
+  fs.readFile("./data.json", (err, text) => {
+    if (err) throw err;
+    let parse = JSON.parse(text);
+
+    fs.readFile("./template.html", (err, template) => {
+      if (err) throw err
+      let temp = template.toString();
+
+      resolve(temp, parse);
+    });
+  });
+
+})
+
+myPromise
+  .then(temp => {
+
   })
-}
-
-
+  .then(temp, parse => {
+    let result = mustache.render(temp, parse);
+    return result;
+    })
+  .then(result => {
+    fs.writeFile("build3.html", result, (err) => {
+      if (err) throw err;
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
