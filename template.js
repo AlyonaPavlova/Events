975e702903a24e3a2b578b1cd6a9c81cbe23f228
Alena Pavlova <alyonabill3@mail.ru>
2018-07-23 23:21:16 +0300

    functions readFile and WriteFile was deleted, function saveFile was changed

--- template.js
+++ template.js
@@ -1,44 +1,16 @@
 "use strict";
 
-const fs = require("fs");
 const mustache = require("mustache");
+const fsPromises = require('fs').promises;
 
 let list = ["./data.json"];
 
-function readFile(path) {
-  return new Promise(resolve => {
-    fs.readFile(path, (err, text) => {
-      if (err) throw err;
-      resolve(text.toString());
-    });
-  });
-}
-
-function writeFile(file, render) {
-  return new Promise(resolve => {
-    fs.writeFile(file, render, (err) => {
-      if (err) throw err;
-      resolve();
-    });
-  });
-}
-
-function render(data, template) {
-  return new Promise(resolve => {
-    resolve(mustache.render(data, template));
-  })
-}
-
 async function saveFile() {
-  let template = await readFile("./template.html");
-  let dataList = await Promise.all(list.map(path => readFile(path)));
-  let data = dataList.reduce(previousValue => {
-    return previousValue;
-  });
+  let template = await fsPromises.readFile("./template.html", "utf8");
+  let dataList = await Promise.all(list.map(path => fsPromises.readFile(path, "utf8")));
+  let data = dataList.reduce(previousValue => {return previousValue;});
 
-  let render = await render(data, template);
-
-  return writeFile("./build6.html", render);
+  await fsPromises.writeFile("./build6.html", mustache.to_html(template, data));
 }
 
 saveFile()
